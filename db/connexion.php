ef6580b433310174859d0d4324c75fe49b45f3f7
Mouha044 <metzogueye712@gmail.com>
2025-09-15 21:40:21 +0000

    Update connexion.php

--- db/connexion.php
+++ db/connexion.php
@@ -1,1 +1,20 @@
+<?php
+$host = "localhost";
+$dbname = "baneservice";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8",
+        $username,
+        $password,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
 
