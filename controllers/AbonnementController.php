890315cd117ff5374af39e74d9e2e31f5b95a2d5
Mouha044 <metzogueye712@gmail.com>
2025-09-16 17:55:21 +0000

    AbonnementController.php

--- controllers/AbonnementController.php
+++ controllers/AbonnementController.php
@@ -6,30 +6,30 @@ class AbonnementController {
 
     public function ajouter($client_id, $date_debut, $date_fin) {
         $abonnement = new Abonnement($client_id, $date_debut, $date_fin);
-        // Ici tu feras l’insertion dans la base de données
+        
         return $abonnement;
     }
 
     public function renouveler($abonnement, $nouvelle_date_fin) {
         $abonnement->setStatut("actif");
-        // Mettre à jour dans la BD
+       
         return $abonnement;
     }
 
     public function suspendre($abonnement) {
         $abonnement->setStatut("suspendu");
-        // Mettre à jour dans la BD
+       
         return $abonnement;
     }
 
     public function payer($abonnement_id, $montant) {
         $paiement = new PaiementAbonnement($abonnement_id, $montant, date("Y-m-d"));
-        // Sauvegarder en BD
+        
         return $paiement;
     }
 
     public function liste() {
-        // Exemple avec PDO, à adapter selon ta structure
+       
         $pdo = new PDO('mysql:host=localhost;dbname=ta_base', 'user', 'password');
         $sql = "SELECT a.id, a.formule, a.prix, a.date_debut, a.date_fin, a.statut, 
                        u.nom, u.prenom
