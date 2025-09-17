9e9a4f97e9828a578188c1005fe39acff1555228
Mouha044 <metzogueye712@gmail.com>
2025-09-17 14:37:52 +0000

    updates pages

--- Includes/navbar.php
+++ Includes/navbar.php
@@ -2,8 +2,10 @@
 <nav>
     <ul>
         <li><a href="/index.php">Accueil</a></li>
-        <li><a href="/services.php">Services</a></li>
         <li><a href="/produits.php">Produits</a></li>
+        <li><a href="/services.php">Services</a></li>
+        <li><a href="/promotions.php">Promotions</a></li>
+        <li><a href="/apropos.php">À propos</a></li>
         <li><a href="/contact.php">Contact</a></li>
         <li><a href="/login.php">Connexion</a></li>
     </ul>
