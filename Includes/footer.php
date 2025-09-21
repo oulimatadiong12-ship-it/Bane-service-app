b9d864fecf796767e5e2c7d0b3406fcffa390585
Mouha044 <metzogueye712@gmail.com>
2025-09-21 02:47:17 +0000

    includes update

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,6 +1,7 @@
 <!-- includes/footer.php -->
 <footer>
-    <p>&copy; <?php echo date("Y"); ?> - Bane Service. Tous droits réservés.</p>
+    <footer class="bg-dark text-white text-center p-3 mt-5">
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
