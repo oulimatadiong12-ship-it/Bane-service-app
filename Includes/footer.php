175f31d95d5fe4f37d69c901961e31c8869e5854
Awson280 <papiaw145@gmail.com>
2025-09-27 02:05:00 +0000

    boonnbbb

--- Includes/footer.php
+++ Includes/footer.php
@@ -1,7 +1,8 @@
 <!-- includes/footer.php -->
-<footer>
-    <footer class="bg-dark text-white text-center p-3 mt-5">
+<footer class="bg-dark text-white text-center p-3 fixed-bottom">
+    &copy; <?= date('Y'); ?> - Mon Application. Tous droits réservés.
 </footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
